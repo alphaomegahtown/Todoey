30a442aa6650b23967e1951c844df6eb184807bb
Luke Ahn <lukeahn@lukes-imac.local>
2018-04-27 23:02:54 -0500

    Convert to MVC design pattern.

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Luke Ahn on 4/27/18.
+//  Copyright © 2018 Luke Ahn. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title : String = ""
+    var done : Bool = false 
+}
